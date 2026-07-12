94394a5dbb43cfec807b4f1be0ca56afc4ab6634
parthpotenza <parth.patel@potenzaglobalsolutions.com>
2026-07-12 18:03:29 +0530

    Implement location, todays date, Icon changes with weather, Humidity and Wind Speed in 'TODAY'S WEATHER CARD'

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 "use strict";
 const APIKey = '9ecb754eb02dcfe338f6b2a0a123e5ff';
 const baseURL = 'https://api.openweathermap.org/data/2.5';
+const iconURL = 'https://openweathermap.org/img/wn';
 const currentAPI = fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=9ecb754eb02dcfe338f6b2a0a123e5ff&units=metric').then((response) => response.json());
 const forcastAPI = fetch('https://api.openweathermap.org/data/2.5/forecast?q=London&appid=9ecb754eb02dcfe338f6b2a0a123e5ff&units=metric').then((response) => response.json());
 
 currentAPI.then((data)=>console.log(data));
 forcastAPI.then((data)=>console.log(data));
+// currentAPI
+// .then((response) => response.json())
+// .then((data) => {
+//     console.log(data);
+//     console.log(data.main.temp);
+//     console.log(data.main.humidity);
+//     console.log(data.wind.speed);
+//     console.log(data.weather[0].description);
+//     console.log(data.weather[0].icon);
+//     console.log("Break");
+// });
+
+
+// forcastAPI
+// .then((response) => response.json())
+// .then((data) => {
+//     console.log(data);
+//     console.log(data.cod);
+//     console.log(data.list[0]);
+// });
 
 //---------- DOM Selection ----------
 //TEMPARATURE UNIT
@@ -37,6 +58,7 @@ const iconSunny = document.getElementById('iconSunny');
 const iconCloudy = document.getElementById('iconCloudy');
 const iconRainy = document.getElementById('iconRainy');
 const iconThunder = document.getElementById('iconThunder');
+const iconSnow = document.getElementById('iconSnow');
 const currentWeather = document.getElementById('currentWeather');
 
 // TODAY'S WEATHER CARD GRID
@@ -96,7 +118,7 @@ const dayFiveWind = document.getElementById('dayFiveWind');
 
 const defaultCity = 'Mumbai';
 
-// --- EVENT LISTENERS ---
+// ---------- EVENT LISTENERS ----------
 
 searchButton.addEventListener('click', function () {
 	fetchWeatherByCity();
@@ -108,8 +130,35 @@ cityInput.addEventListener('keydown', function (e) {
 	}
 });
 
+closeAlert.addEventListener('click', function () {
+	hideAlert();
+});
 
-// FETCH WEATHER BY CITY
+// myLocation.addEventListener('click', function () {
+// 	requestGeolocation();
+// });
+
+// btnC.addEventListener('click', function () {
+// 	toggleTemperature('metric');
+// });
+
+// btnF.addEventListener('click', function () {
+// 	toggleTemperature('imperial');
+// });
+
+// recentSearches.addEventListener('click', function () {
+// 	toggleDropdown();
+// });
+
+// document.addEventListener('click', function (e) {
+// 	if (!recentWrapper.contains(e.target)) {
+// 		closeDropdown();
+// 	}
+// });
+
+
+
+// ---------- FETCH WEATHER BY CITY ----------
 
 function fetchWeatherByCity(addedCity) {
 	let city;
@@ -171,12 +220,84 @@ function fetchWeatherByCity(addedCity) {
 	hideError();
 }
 
-// SHOW ERROR
+// ---------- SHOW ERROR ----------
 function showError(message) {
 	errorMsg.classList.remove('hidden');
 	errorMsg.textContent = message;
 }
 
+// ---------- HIDE ERROR ----------
 function hideError() {
 	errorMsg.classList.add('hidden');
+}
+
+// ---------- DISPLAY CURRENT WEATHER ----------
+function displayCurrentWeather(data) {
+	
+	mainLocation.innerHTML = data.name + ', ' + data.sys.country;
+	dateToday.innerHTML = formatedDate();
+	mainTemp.innerHTML = Math.round(data.main.temp);
+	currentWeather.innerHTML = data.weather[0].description;
+	humidity.innerHTML = data.main.humidity + " %";
+	windSpeed.innerHTML = Math.round(data.wind.speed) + " m/s";
+
+	// Temperature Alert
+	hideAlert();
+	if (data.main.temp > 40) {
+		exceedTemp.innerHTML = 40;
+		showAlert()
+	}
+
+	// Icon changes with weather condition
+	if (data.weather[0].icon === '01d' || data.weather[0].icon === '01n') {
+		iconSunny.classList.remove('hidden');
+		iconCloudy.classList.add('hidden');
+		iconRainy.classList.add('hidden');
+		iconThunder.classList.add('hidden');
+		iconSnow.classList.add('hidden');
+	} else if (data.weather[0].icon === '02d' || data.weather[0].icon === '02n' || data.weather[0].icon === '03n' || data.weather[0].icon === '03n' || data.weather[0].icon === '04d' || data.weather[0].icon === '04n') {
+		iconSunny.classList.add('hidden');
+		iconCloudy.classList.remove('hidden');
+		iconRainy.classList.add('hidden');
+		iconThunder.classList.add('hidden');
+		iconSnow.classList.add('hidden');
+	} else if (data.weather[0].icon === '09d' || data.weather[0].icon === '09n' || data.weather[0].icon === '10n' || data.weather[0].icon === '10n') {
+		iconSunny.classList.add('hidden');
+		iconCloudy.classList.add('hidden');
+		iconRainy.classList.remove('hidden');
+		iconThunder.classList.add('hidden');
+		iconSnow.classList.add('hidden');
+	} else if (data.weather[0].icon === '11d' || data.weather[0].icon === '11n') {
+		iconSunny.classList.add('hidden');
+		iconCloudy.classList.add('hidden');
+		iconRainy.classList.add('hidden');
+		iconThunder.classList.remove('hidden');
+		iconSnow.classList.add('hidden');
+	} else {
+		iconSunny.classList.add('hidden');
+		iconCloudy.classList.add('hidden');
+		iconRainy.classList.add('hidden');
+		iconThunder.classList.add('hidden');
+		iconSnow.classList.remove('hidden');
+		console.log(data.weather[0].icon);
+	}
+	
+}
+
+// ---------- DATE FORMAT ----------
+function formatedDate() {
+	let date = new Date();
+	return date.toLocaleString('en-US', {
+		weekday: 'long', year: 'numeric', month: 'long', day: 'numeric',
+	});
+}
+
+// ---------- SHOW ALERT ----------
+function showAlert() {
+	alertBanner.classList.remove('hidden');
+}
+
+// ---------- HIDE ALERT ----------
+function hideAlert() {
+	alertBanner.classList.add('hidden');
 }
